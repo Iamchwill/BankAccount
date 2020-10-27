14f9f2e887cf42f7e759742055fca1e57e598447
A2c4u6t <wchen20@stuy.edu>
2020-10-27 19:53:13 -0400

    Added the constructor for BankAccount and the variables

---
+++ Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+  public static void main(String[] args) {
+    BankAccount a1 = new BankAccount(12345, "54321");
+    BankAccount a2 = new BankAccount(19876, "67891");
+  }
+}
