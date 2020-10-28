7ad075f4bcaa3bab2ff11536884db01d7d66a9e6
A2c4u6t <wchen20@stuy.edu>
2020-10-27 23:06:57 -0400

    Added the withdraw and deposit methods

--- BankAccount.java
+++ BankAccount.java
@@ -20,9 +20,25 @@ public class BankAccount {
   public void setPassword(String newPass) {
     password = newPass;
   }
-
-  public String getPass() {
+  /*public String getPass() {
     return password;
   }
-  
+  */
+
+  public boolean deposit(double amount) {
+      if (amount > 0) {
+        balance += amount;
+        return true;
+      }
+      return false;
+    }
+
+    public boolean withdraw(double amount) {
+      if (amount > 0 && amount < balance) {
+        balance -= amount;
+        return true;
+      }
+      return false;
+    }
+
 }
--- Tester.java
+++ Tester.java
@@ -4,8 +4,17 @@ public class Tester {
     BankAccount a2 = new BankAccount(19876, "67891");
     System.out.println(a1.getBalance());
     System.out.println(a2.getAccountID());
-    System.out.println(a1.getPass());
+    //System.out.println(a1.getPass());
     a1.setPassword("00000");
-    System.out.println(a1.getPass());
+    //System.out.println(a1.getPass());
+    System.out.println(a1.getBalance());
+    System.out.println(a2.getAccountID());
+    System.out.println(a1.deposit(1000));
+    System.out.println(a1.getBalance());
+    System.out.println(a1.withdraw(500));
+    System.out.println(a1.getBalance());
+    System.out.println(a2.withdraw(50));
+    System.out.println(a2.deposit(100));
+    System.out.println(a2.getBalance());
   }
 }
